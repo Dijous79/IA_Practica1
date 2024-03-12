04d427d18600c209c7707dad2fd86268858d2874
ElJorge2 <jordi.vives.miro@estudiantat.upc.edu>
2024-03-12 12:59:05 +0100

    Creadora DFSBoard

--- codes/DFSBoard.java
+++ codes/DFSBoard.java
@@ -2,6 +2,19 @@ package codes;
 
 import IA.DistFS.*;
 
+import java.util.Arrays;
+
 public class DFSBoard {
-    
+    Servers servers;
+    Requests requests;
+    int[][] assignacio;
+    public DFSBoard(int nServers, int mReps, int nUsers, int mConsults, int seed) throws Servers.WrongParametersException {
+        servers = new Servers(nServers, mReps, seed);
+        requests = new Requests(nUsers, mConsults, seed);
+        assignacio = new int[nUsers][mConsults];
+        assignacioInicial();
+    }
+    private void assignacioInicial() {
+        for (int[] is : assignacio) Arrays.fill(is, 0);
+    }
 }
