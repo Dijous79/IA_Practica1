ba4de653b8157acb99ef1d8e000cdf9590bd7512
jowelhub <146464443+jowelhub@users.noreply.github.com>
2024-03-12 15:28:58 +0100

    heuristicaaaa

--- codes/DFSHeuristicFunction.java
+++ codes/DFSHeuristicFunction.java
@@ -6,9 +6,32 @@ import aima.search.framework.HeuristicFunction;
 
 public class DFSHeuristicFunction implements HeuristicFunction {
 
-	public double getHeuristicValue(Object state){
-
-		return 0.0;
+	public boolean equals(Object obj) {
+		boolean retValue;
+		
+		retValue = super.equals(obj);
+		return retValue;
 	}
+
+	public double getHeuristicValue(Object state) {
+		DFSBoard board = (DFSBoard) state;
+		int ns = board.getNServersQueSutilitzen();
+		//osigui el size dels "contenidors" no buits
+		
+		double mean = 0;
+		double variance = 0;
+	
+		for (int i = 0; i < ns; i++) {
+			double serverTotalTime = board.getServerTotalTime(i);
+			mean += serverTotalTime;
+			variance += Math.pow(serverTotalTime, 2);
+		}
+	
+		mean /= ns;
+		variance = variance / ns - Math.pow(mean, 2);
+	
+		return (mean + variance);
+	}
+	
 
 }
