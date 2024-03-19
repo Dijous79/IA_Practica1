26b972b822757fbb8353c5cfcbce0f44658f6ddd
jan.chaler <jan.chaler@estudiantat.upc.edu>
2024-03-19 10:27:34 +0100

    Main

--- Main.java
+++ Main.java
@@ -1,7 +1,26 @@
+import IA.DistFS.Servers;
+import codes.DFSBoard;
+
+import java.util.Scanner;
 
 public class Main {
-    int nS, nU, mR, mC, s;
+    public static void main(String[] args) throws Servers.WrongParametersException {
+        int nS, nU, mR, mC, s;
+        Scanner scn = new Scanner(System.in);
+        System.out.println("input: nº of Servers:");
+        nS = scn.nextInt();
+        System.out.println("input: nº of Users:");
+        nU = scn.nextInt();
+        System.out.println("input: nº of minimumRepeated files:");
+        mR = scn.nextInt();
+        System.out.println("input: nº of maximumRequests for user:");
+        mC = scn.nextInt();
+        System.out.println("input: random seed:");
+        s = scn.nextInt();
 
 
 
+        DFSBoard prova = new DFSBoard(nS, mR, nU, mC, s);
+        prova.pintaConsultes();
+    }
 }
